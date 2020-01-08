20503af53010e28238f3304e69e5592aa9b76855
roberthshields93 <roberthshields93@yahoo.com>
2020-01-08 14:25:03 -0800

    Created student DB

---
+++ BranchingExample/DBScripts/CreateStudentDB.sql
@@ -0,0 +1,19 @@
+﻿use master
+GO 
+
+drop database Student
+GO
+
+CREATE DATABASE Student
+GO
+
+USE Student
+GO
+
+CREATE TABLE Student
+(
+	FirstName		varchar(25)		NOT NULL
+	,LastName		varchar(25)		NOT NULL
+	,DateOfBirth	date			NULL
+	,CellPhone		char(10)		NULL
+)
